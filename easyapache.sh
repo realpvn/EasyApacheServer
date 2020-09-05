c713d41955ed0376d8dd319853f416e386b18ae5
Pavan Kumar <pavank@sostronk.com>
2020-09-05 15:50:57 +0530

    script: provide execute permission for scripts

--- easyapache.sh
+++ easyapache.sh
@@ -43,16 +43,20 @@ sudo apt autoremove -y && sudo apt autoclean -y
 IP=`curl -s icanhazip.com`
 echo -e "Server Public IP: ${Purple}"${IP}${Rst}
 
+$scriptDir="./scripts"
+chmod +x $scriptDir/server.sh
+chmod +x $scriptDir/ssl.sh
+
 while getopts 'fas' flag; do
   case ${flag} in
-    f ) ./scripts/server.sh
-        ./scripts/ssl.sh
+    f ) $scriptDir/server.sh
+        $scriptDir/ssl.sh
         exit;;
-	a ) ./scripts/server.sh
+	a ) $scriptDir/server.sh
 		exit;;
-	s ) ./scripts/ssl.sh
+	s ) $scriptDir/ssl.sh
 		exit;;
-    * ) echo -e "${Green}Usage: easy-apache [options]\n-f:\t(default) Full setup, default option if none is provided\n-a:\tAdding new site (includes apache install)\n-s:\tInstall SSL certificate for sites available${Rst}"
+    * ) echo -e "${Green}Usage: easy-apache [options]\nOptions:\n-f:\t(default) Full setup, default option if none is provided\n-a:\tAdding new site (includes apache install)\n-s:\tInstall SSL certificate for sites available${Rst}"
        	echo -e "Example\n./easyapache -f   #for full installation i.e Apache & SSL certificate\n./easyapache -as  #for installating Apache server & SSL certificate"
 		exit;;
   esac
