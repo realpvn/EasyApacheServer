b289023ebaae841f5fdbfe1aa33704404bd672b7
Pavan Kumar <pavank@sostronk.com>
2020-09-06 08:48:28 +0530

    script: updates as per testing

--- easyapache.sh
+++ easyapache.sh
@@ -13,13 +13,13 @@ while getopts 'fash' flag; do
 		exit;;
 	s ) $scriptDir/ssl.sh
 		exit;;
-	h ) echo -e "Usage: easy-apache [options]\nOptions:\n-f:\t(default) Full setup, default option if none is provided\n-a:\tAdding new site (includes apache install)\n-s:\tInstall SSL certificate for sites available\n-h:\tHelp (shows available commands)${Rst}"
+	h ) echo -e "Usage: easy-apache [options]\nOptions:\n-f:\tFull setup, default option if none is provided\n-a:\tAdding new site (includes apache install)\n-s:\tInstall SSL certificate for sites available\n-h:\tHelp (shows available commands)${Rst}"
        	echo -e "\nExample\n./easyapache -f   #for full installation i.e Apache & SSL certificate\n./easyapache -as  #for installating Apache server & SSL certificate"
 		exit;;
-    * ) echo -e "Usage: easy-apache [options]\nOptions:\n-f:\t(default) Full setup, default option if none is provided\n-a:\tAdding new site (includes apache install)\n-s:\tInstall SSL certificate for sites available\n-h:\tHelp (shows available commands)${Rst}"
+    * ) echo -e "Usage: easy-apache [options]\nOptions:\n-f:\tFull setup, default option if none is provided\n-a:\tAdding new site (includes apache install)\n-s:\tInstall SSL certificate for sites available\n-h:\tHelp (shows available commands)${Rst}"
        	echo -e "\nExample\n./easyapache -f   #for full installation i.e Apache & SSL certificate\n./easyapache -as  #for installating Apache server & SSL certificate"
 		exit;;
   esac
 done
-echo -e "Usage: easy-apache [options]\n-f:\t(default) Full setup, default option if none is provided\n-a:\tAdding new site (includes apache install)\n-s:\tInstall SSL certificate for sites available\n-h:\tHelp (shows available commands)${Rst}"
+echo -e "Usage: easy-apache [options]\n-f:\tFull setup, default option if none is provided\n-a:\tAdding new site (includes apache install)\n-s:\tInstall SSL certificate for sites available\n-h:\tHelp (shows available commands)${Rst}"
 echo -e "\nExample\n./easyapache -f   #for full installation i.e Apache & SSL certificate\n./easyapache -as  #for installating Apache server & SSL certificate"
