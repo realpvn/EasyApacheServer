13074336ee9d1e2c58a64ab223d1fba132176266
Pavan Kumar <pavank@sostronk.com>
2020-09-06 22:35:48 +0530

    script: remove scriptDir

--- easy-apache.sh
+++ easy-apache.sh
@@ -1,17 +1,16 @@
 source rainbow.sh
 
-scriptDir="./scripts"
-chmod +x $scriptDir/server.sh
-chmod +x $scriptDir/ssl.sh
+chmod +x server.sh
+chmod +x ssl.sh
 
 while getopts 'fash' flag; do
   case ${flag} in
-    f ) $scriptDir/server.sh
-        $scriptDir/ssl.sh
+    f ) ./server.sh
+        ./ssl.sh
         exit;;
-	a ) $scriptDir/server.sh
+	a ) ./server.sh
 		exit;;
-	s ) $scriptDir/ssl.sh
+	s ) ./ssl.sh
 		exit;;
 	h ) echo -e "Usage: easy-apache [options]\nOptions:\n-f:\tFull setup, default option if none is provided\n-a:\tAdding new site (includes apache install)\n-s:\tInstall SSL certificate for sites available\n-h:\tHelp (shows available commands)${Rst}"
        	echo -e "\nExample\n./easy-apache -f   #for full installation i.e Apache & SSL certificate\n./easy-apache -as  #for installating Apache server & SSL certificate"
