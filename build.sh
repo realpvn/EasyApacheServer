d0a1d207d096880db078fa944bfd5265b36dc5a1
Pavan Kumar <pavank@sostronk.com>
2020-09-26 14:08:53 +0530

    script: changes and added new build script

---
+++ build.sh
@@ -0,0 +1,56 @@
+clean () {
+    echo "Cleaning build"
+    rm easy-apache ../easy-apache_*
+    git checkout debian/changelog
+}
+
+build () {
+    echo "Building Debian Package"
+    clean
+    cp easy-apache.sh easy-apache
+    oldVer=`head -1 debian/changelog | cut -d '(' -f2 | cut -d ')' -f1`
+    echo Old Version: $oldVer
+    read -p "New Version: " ver
+    dch -v $ver
+    curVer=`head -1 debian/changelog | cut -d '(' -f2 | cut -d ')' -f1 | cut -d '-' -f1`
+    dh_make -p easy-apache_$curVer --indep --createorig -c gpl3 -e realpvn@gmail.com
+}
+
+source () {
+    build
+    debuild -S
+}
+
+binary () {
+    build
+    debuild
+}
+
+upload () {
+    curVer=`head -1 debian/changelog | cut -d '(' -f2 | cut -d ')' -f1`
+    echo "Current version: $curVer"
+    read -p "Is the current version correct? (Y/N): " verCheck
+    case $verCheck in
+        [Yy]* ) echo "Starting Upload"
+                cd ..
+                dput ppa:realpvn/easy-apache easy-apache_$$curVer_source.changes;;
+        [Nn]* ) echo "Terminating upload";;
+            * ) echo "Wrong input";;
+    esac
+}
+
+while getopts 'scub' flag
+do
+    case $flag in 
+        s ) source
+            exit;;
+        c ) clean
+            exit;;
+        u ) upload
+            exit;;
+        b ) binary
+            exit;;
+        * ) echo "Use option -s to build deb source Package"
+    esac
+done
+echo "Use option -s to build deb source Package"
